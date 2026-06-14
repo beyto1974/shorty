db32e9835201f7d6e31c64b2b0b8e2352470bc5f
beyto1974 <beytobeyto1974@gmail.com>
2026-06-14 11:47:49 +0000

    Unnecessary transaction removal

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/shortener', [ShortenerController::class, 'put'])->middleware(DBTransactionMiddleware::class);
     Route::get('/shortener/{shortener}', [ShortenerController::class, 'get'])->middleware(OwnerMiddleware::class);
     Route::delete('/shortener/{shortener}', [ShortenerController::class, 'delete'])->middleware([OwnerMiddleware::class, DBTransactionMiddleware::class]);
-    Route::post('/shortener/search', [ShortenerController::class, 'search'])->middleware([DBTransactionMiddleware::class]);
+    Route::post('/shortener/search', [ShortenerController::class, 'search']);
 
     Route::get('/user/stats', [StatsController::class, 'getUserStats']);
 });
 
 Route::middleware(MasterTokenAuth::class)->group(function () {
-    Route::get('/stats', [StatsController::class, 'getGlobalStats'])->middleware(DBTransactionMiddleware::class);
+    Route::get('/stats', [StatsController::class, 'getGlobalStats']);
 });
